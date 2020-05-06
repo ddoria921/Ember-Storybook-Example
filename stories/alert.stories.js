f19b9155442d62ed6f52a517040019acb24b87fa
Darin Doria <ddoria921@gmail.com>
2020-05-06 19:25:29 -0400

    Tweak formatting of Alert story

--- stories/alert.stories.js
+++ stories/alert.stories.js
@@ -12,18 +12,34 @@ export const Default = () => (
 
 export const Themes = () => (
   hbs`
-    <Alert @heading="Hello World" @body="An example of what an info alert looks like." @type="info"/>
+    <Alert
+      @heading="Hello World"
+      @body="An example of what an info alert looks like."
+      @type="info"
+    />
 
     <div class="my-6"></div>
 
-    <Alert @heading="Success!" @body="An example of what a success alert looks like." @type="success"/>
+    <Alert
+      @heading="Success!"
+      @body="An example of what a success alert looks like."
+      @type="success"
+    />
 
     <div class="my-6"></div>
 
-    <Alert @heading="Warning" @body="An example of what a warning alert looks like." @type="warning"/>
+    <Alert
+      @heading="Warning"
+      @body="An example of what a warning alert looks like."
+      @type="warning"
+    />
 
     <div class="my-6"></div>
 
-    <Alert @heading="Uh oh..." @body="An example of what an error alert looks like." @type="danger"/>
+    <Alert
+      @heading="Uh oh..."
+      @body="An example of what an error alert looks like."
+      @type="danger"
+    />
   `
 );
