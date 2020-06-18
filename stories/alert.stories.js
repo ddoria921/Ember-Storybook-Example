2ce5bab108c5a57985b1c372f7852ca40495c2b8
Darin Doria <ddoria921@gmail.com>
2020-06-18 08:35:46 -0400

    Ending of second video

---
+++ stories/alert.stories.js
@@ -0,0 +1,31 @@
+import { hbs } from 'ember-cli-htmlbars';
+
+// <Alert @heading="Hello world" @body="More information" @type="info"/>
+
+export default {
+  title: 'Alert'
+}
+
+export const Default = () =>
+  hbs`
+    <Alert @heading="Hello world"/>
+  `;
+
+export const withBody = () =>
+  hbs`
+    <Alert
+      @heading="Hello world"
+      @body="More information..."
+    />
+  `;
+
+export const Themes = () =>
+  hbs`
+    {{#each (array "info" "warning" "danger" "success") as |theme|}}
+      <Alert
+        class="mb-6"
+        @heading={{theme}}
+        @type={{theme}}
+      />
+    {{/each}}
+  `;
