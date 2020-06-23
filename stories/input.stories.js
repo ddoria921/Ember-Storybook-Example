224bf3460fafc18c36971598789a992cdf0712d7
Darin Doria <ddoria921@gmail.com>
2020-06-23 12:31:42 -0400

    Updated end state for 3rd video

--- stories/input.stories.js
+++ stories/input.stories.js
@@ -1,5 +1,5 @@
 import { hbs } from 'ember-cli-htmlbars'
-import { action } from '@storybook/addon-actions';
+import { set } from '@ember/object'
 
 export default {
   title: 'Input'
@@ -12,16 +12,33 @@ export const Default = () => ({
       @value={{this.email}}
       @label="Email"
       @type="email"
-      @enterPressed={{action this.handleEnterPress}}
-      @onChange={{action this.handleInput}}
+      @onChange={{action this.onChange}}
     />
   `,
   context: {
     email: '',
-    handleEnterPress: action('enterPressed'),
-    handleInput(value) {
+    onChange(value) {
+      set(this, 'email', value);
+    }
+  }
+});
+
+export const errorState = () => ({
+  template: hbs`
+    <XInput
+      placeholder="username"
+      @value={{this.email}}
+      @label="Username (8 character max)"
+      @isError={{this.isError}}
+      @errorMessage="Username can't be longer than 8 characters"
+      @onChange={{action this.onChange}}
+    />
+  `,
+  context: {
+    email: '',
+    onChange(value) {
+      set(this, 'isError', value.length > 8);
       this.set('email', value);
-      action('onChange')(...arguments);
     }
   }
 })
