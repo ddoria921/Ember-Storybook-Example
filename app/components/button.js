a45da4703fc305dc05ba74a169083e890a316641
Darin Doria <ddoria921@gmail.com>
2020-08-19 20:20:30 -0400

    Add button theme option

--- app/components/button.js
+++ app/components/button.js
@@ -2,6 +2,23 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
 export default class ButtonComponent extends Component {
+  get theme() {
+    return this.args.theme || 'info';
+  }
+
+  get themeColor() {
+    switch (this.theme) {
+      case 'success':
+        return 'green';
+      case 'danger':
+        return 'red';
+      case 'info':
+        return 'blue';
+      default:
+        return 'blue';
+    }
+  }
+
   @action
   onClick() {
     if (this.args.onClick) {
